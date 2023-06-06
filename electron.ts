4829f211e5f5ccb6a28ae86b7e8d21e741eb1858
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2023-06-06 17:11:56 -0400

    Update electron.ts

--- electron.ts
+++ electron.ts
@@ -3,8 +3,8 @@ const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 300,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
